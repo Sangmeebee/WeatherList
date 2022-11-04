1be94222f62686c1753ada150a8146f32e8a63e4
Sangmeebee <apfhdznzl@gmail.com>
2022-11-04 15:02:41 +0900

    feat: NetworkModule 제작

---
+++ remote/src/main/java/com/sangmeebee/weatherlist/remote/di/NetworkModule.kt
@@ -0,0 +1,48 @@
+package com.sangmeebee.weatherlist.remote.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Converter
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal object NetworkModule {
+
+    private const val SEARCH_WEATHER_BASE_URL = "https://api.openweathermap.org/"
+
+    @Singleton
+    @Provides
+    fun provideConverter(): Converter.Factory = GsonConverterFactory.create()
+
+    @Singleton
+    @Provides
+    fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor =
+        HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+
+    @Singleton
+    @Provides
+    fun provideOkhttpClient(
+        httpLoggingInterceptor: HttpLoggingInterceptor,
+    ): OkHttpClient =
+        OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .build()
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(
+        converter: Converter.Factory,
+        okHttpClient: OkHttpClient,
+    ): Retrofit = Retrofit.Builder()
+        .client(okHttpClient)
+        .baseUrl(SEARCH_WEATHER_BASE_URL)
+        .addConverterFactory(converter)
+        .build()
+}
