960bd4cd9c44bd7aad8a61a31e1ccc550b8a9577
Sangmeebee <apfhdznzl@gmail.com>
2022-11-05 03:20:40 +0900

    feat: 위치 데이터소스에서 발생되는 예외 처리

--- remote/src/main/java/com/sangmeebee/weatherlist/remote/datasource/GeocoderRemoteDatasourceImpl.kt
+++ remote/src/main/java/com/sangmeebee/weatherlist/remote/datasource/GeocoderRemoteDatasourceImpl.kt
@@ -3,6 +3,13 @@ package com.sangmeebee.weatherlist.remote.datasource
 import com.sangmeebee.weatherlist.data.datasource.remote.GeocoderRemoteDatasource
 import com.sangmeebee.weatherlist.data.di.IoDispatcher
 import com.sangmeebee.weatherlist.data.model.LocationEntity
+import com.sangmeebee.weatherlist.remote.exceptions.DisConnectNetworkException
+import com.sangmeebee.weatherlist.remote.exceptions.EMPTY_RESULT_LOCATION
+import com.sangmeebee.weatherlist.remote.exceptions.EmptyResultLocationException
+import com.sangmeebee.weatherlist.remote.exceptions.ILLEGAL_APP_TOKEN
+import com.sangmeebee.weatherlist.remote.exceptions.ILLEGAL_LOCATION
+import com.sangmeebee.weatherlist.remote.exceptions.IllegalAppTokenException
+import com.sangmeebee.weatherlist.remote.exceptions.IllegalLocationException
 import com.sangmeebee.weatherlist.remote.service.GeocoderAPI
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
@@ -12,9 +19,16 @@ internal class GeocoderRemoteDatasourceImpl @Inject constructor(
     private val geocoderAPI: GeocoderAPI,
     @IoDispatcher private val ioDispatcher: CoroutineDispatcher,
 ) : GeocoderRemoteDatasource {
-    override suspend fun getLocation(zipCode: String, appId: String): Result<LocationEntity> = runCatching {
+    override suspend fun getLocation(zipCode: String, appId: String): Result<LocationEntity> = try {
         withContext(ioDispatcher) {
-            geocoderAPI.getLocation(zipCode, appId).toData()
+            Result.success(geocoderAPI.getLocation(zipCode, appId).toData())
+        }
+    } catch (e: Exception) {
+        when {
+            e.message?.contains(ILLEGAL_LOCATION) == true -> Result.failure(IllegalLocationException())
+            e.message?.contains(EMPTY_RESULT_LOCATION) == true -> Result.failure(EmptyResultLocationException())
+            e.message?.contains(ILLEGAL_APP_TOKEN) == true -> Result.failure(IllegalAppTokenException())
+            else -> Result.failure(DisConnectNetworkException())
         }
     }
 }
