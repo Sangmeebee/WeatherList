d708db2aee2f5248aa431907c52f93f9a6c501f2
Sangmeebee <apfhdznzl@gmail.com>
2022-11-05 03:20:23 +0900

    feat: 데이터 통신에서 발생되는 예외 정의

---
+++ remote/src/main/java/com/sangmeebee/weatherlist/remote/exceptions/Exceptions.kt
@@ -0,0 +1,10 @@
+package com.sangmeebee.weatherlist.remote.exceptions
+
+class IllegalLocationException : IllegalArgumentException()
+class EmptyResultLocationException : IllegalArgumentException()
+class IllegalAppTokenException : IllegalArgumentException()
+class DisConnectNetworkException : IllegalArgumentException()
+
+const val ILLEGAL_LOCATION = "400"
+const val EMPTY_RESULT_LOCATION = "404"
+const val ILLEGAL_APP_TOKEN = "401"
