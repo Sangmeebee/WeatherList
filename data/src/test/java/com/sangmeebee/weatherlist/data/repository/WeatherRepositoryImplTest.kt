293adf0f2c4d1c3b94807f1120a8b8c5fb2cbcd9
Sangmeebee <apfhdznzl@gmail.com>
2022-11-04 20:30:54 +0900

    test: 날씨 레포지토리 테스트 코드 구현 & 테스트

--- data/src/test/java/com/sangmeebee/weatherlist/data/repository/WeatherRepositoryImplTest.kt
+++ data/src/test/java/com/sangmeebee/weatherlist/data/repository/WeatherRepositoryImplTest.kt
@@ -2,14 +2,18 @@ package com.sangmeebee.weatherlist.data.repository
 
 import com.sangmeebee.weatherlist.data.datasource.remote.WeatherRemoteDatasource
 import com.sangmeebee.weatherlist.domain.repository.WeatherRepository
+import io.mockk.coEvery
+import io.mockk.coVerify
 import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Test
 
 class WeatherRepositoryImplTest {
 
-    lateinit var weatherRemoteDatasource: WeatherRemoteDatasource
-    lateinit var weatherRepository: WeatherRepository
+    private lateinit var weatherRemoteDatasource: WeatherRemoteDatasource
+    private lateinit var weatherRepository: WeatherRepository
 
     @Before
     fun setUp() {
@@ -17,10 +21,16 @@ class WeatherRepositoryImplTest {
         weatherRepository = WeatherRepositoryImpl(weatherRemoteDatasource)
     }
 
+    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
-    fun `날씨정보를 호출한다`() {
+    fun `날씨정보를 호출한다`() = runTest {
         // given
+        coEvery {
+            weatherRepository.getWeather(latitude = 37.5666791, longitude = 126.9782914, appId = "appId")
+        } returns runCatching { listOf() }
         // when
+        weatherRepository.getWeather(latitude = 37.5666791, longitude = 126.9782914, appId = "appId")
         // then
+        coVerify { weatherRemoteDatasource.getWeather(latitude = 37.5666791, longitude = 126.9782914, appId = "appId") }
     }
 }
