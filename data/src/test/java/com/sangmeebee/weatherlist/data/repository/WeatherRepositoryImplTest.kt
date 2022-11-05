948d0ffd3b6529051094a0ed4eaf310b20d630de
Sangmeebee <apfhdznzl@gmail.com>
2022-11-05 18:04:44 +0900

    test: 날씨 레퍼지토리 기능 추가에 따른 테스트 코드를 작성

--- data/src/test/java/com/sangmeebee/weatherlist/data/repository/WeatherRepositoryImplTest.kt
+++ data/src/test/java/com/sangmeebee/weatherlist/data/repository/WeatherRepositoryImplTest.kt
@@ -1,11 +1,13 @@
 package com.sangmeebee.weatherlist.data.repository
 
+import com.sangmeebee.weatherlist.data.datasource.local.WeatherLocalDatasource
 import com.sangmeebee.weatherlist.data.datasource.remote.WeatherRemoteDatasource
 import com.sangmeebee.weatherlist.domain.repository.WeatherRepository
 import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Test
@@ -13,12 +15,14 @@ import org.junit.Test
 class WeatherRepositoryImplTest {
 
     private lateinit var weatherRemoteDatasource: WeatherRemoteDatasource
+    private lateinit var weatherLocalDatasource: WeatherLocalDatasource
     private lateinit var weatherRepository: WeatherRepository
 
     @Before
     fun setUp() {
         weatherRemoteDatasource = mockk()
-        weatherRepository = WeatherRepositoryImpl(weatherRemoteDatasource)
+        weatherLocalDatasource = mockk()
+        weatherRepository = WeatherRepositoryImpl(weatherRemoteDatasource, weatherLocalDatasource)
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
@@ -32,5 +36,53 @@ class WeatherRepositoryImplTest {
         weatherRepository.getWeather(latitude = 37.5666791, longitude = 126.9782914, appId = "appId")
         // then
         coVerify { weatherRemoteDatasource.getWeather(latitude = 37.5666791, longitude = 126.9782914, appId = "appId") }
+    }
+
+    @Test
+    fun `캐시에 날씨정보를 저장한다`() = runTest {
+        // given
+        coEvery {
+            weatherRepository.insertAllAtCache(emptyList())
+        } returns Result.success(Unit)
+        // when
+        weatherRepository.insertAllAtCache(emptyList())
+        // then
+        coVerify { weatherLocalDatasource.insertAll(emptyList()) }
+    }
+
+    @Test
+    fun `캐시에 날씨정보를 삭제한다`() = runTest {
+        // given
+        coEvery {
+            weatherRepository.deleteWeathersAtCache(emptyList())
+        } returns Result.success(Unit)
+        // when
+        weatherRepository.deleteWeathersAtCache(emptyList())
+        // then
+        coVerify { weatherLocalDatasource.deleteWeathers(emptyList()) }
+    }
+
+    @Test
+    fun `캐시 저장된 날씨정보를 호출한다`() = runTest {
+        // given
+        coEvery {
+            weatherRepository.getAllWeathersAtCache()
+        } returns Result.success(emptyMap())
+        // when
+        weatherRepository.getAllWeathersAtCache()
+        // then
+        coVerify { weatherLocalDatasource.getAllWeathers() }
+    }
+
+    @Test
+    fun `캐시에 저장된 날씨정보를 데이터 스트림으로 반환한다`() = runTest {
+        // given
+        coEvery {
+            weatherRepository.getCacheWeathersFlow()
+        } returns flowOf()
+        // when
+        weatherRepository.getCacheWeathersFlow()
+        // then
+        coVerify { weatherLocalDatasource.getWeathersFlow() }
     }
 }
