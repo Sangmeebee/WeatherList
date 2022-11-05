c7e3f1167a674c2107275e7c2d54016b0d74bd94
Sangmeebee <apfhdznzl@gmail.com>
2022-11-06 00:34:36 +0900

    feat: 현재 날짜 가져오는 유스케이스 구현

--- domain/src/main/java/com/sangmeebee/weatherlist/domain/usecase/GetCurrentDateUsecase.kt
+++ domain/src/main/java/com/sangmeebee/weatherlist/domain/usecase/GetCurrentDateUsecase.kt
@@ -1,3 +1,17 @@
 package com.sangmeebee.weatherlist.domain.usecase
 
-class GetCurrentDateUsecase
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
+
+class GetCurrentDateUsecase {
+    operator fun invoke(): String {
+        val currentDate = Calendar.getInstance().time
+        val dateFormat = SimpleDateFormat(DATE_FORMAT_PATTERN, Locale.getDefault())
+        return dateFormat.format(currentDate)
+    }
+
+    companion object {
+        private const val DATE_FORMAT_PATTERN = "yyyy-MM-dd"
+    }
+}
