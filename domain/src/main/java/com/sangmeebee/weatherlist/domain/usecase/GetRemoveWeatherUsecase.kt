64430b2ba6b62245399696f0ee9fa48ae35e44d3
Sangmeebee <apfhdznzl@gmail.com>
2022-11-06 01:14:19 +0900

    feat: 과거의 데이터거나 최대 출력 개수를 초과해 로컬데이터베이스에서 제거해야하는 데이터를 반환하는 유스케이스 작성

--- domain/src/main/java/com/sangmeebee/weatherlist/domain/usecase/GetRemoveWeatherUsecase.kt
+++ domain/src/main/java/com/sangmeebee/weatherlist/domain/usecase/GetRemoveWeatherUsecase.kt
@@ -1,7 +1,25 @@
 package com.sangmeebee.weatherlist.domain.usecase
 
 import com.sangmeebee.weatherlist.domain.model.Weather
+import com.sangmeebee.weatherlist.domain.usecase.FetchWeatherUsecase.Companion.WEATHER_MAX_COUNT
+import javax.inject.Inject
 
-class GetRemoveWeatherUsecase {
-    operator fun invoke(weathers: List<Weather>): List<Weather> = emptyList()
+class GetRemoveWeatherUsecase @Inject constructor(
+    private val getCurrentDateUsecase: GetCurrentDateUsecase,
+    private val convertTimestampToDateUsecase: ConvertTimestampToDateUsecase,
+) {
+    operator fun invoke(weathers: List<Weather>): List<Weather> {
+        var existCount = WEATHER_MAX_COUNT
+
+        val oldWeathers = weathers.filterIndexed { index, weather ->
+            val oldDate = convertTimestampToDateUsecase(weather.timestamp)
+            val currentDate = getCurrentDateUsecase()
+            val isOldDate = oldDate < currentDate
+            if (isOldDate) {
+                existCount += 1
+            }
+            isOldDate || index >= existCount
+        }
+        return oldWeathers
+    }
 }
