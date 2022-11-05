0fd97a9c23fcc328a88339fa28478944d5680aad
Sangmeebee <apfhdznzl@gmail.com>
2022-11-06 01:14:03 +0900

    test: 과거의 데이터거나 최대 출력 개수를 초과해 로컬데이터베이스에서 제거해야하는 데이터를 반환하는 유스케이스 테스트 코드 작성

--- domain/src/test/java/com/sangmeebee/weatherlist/domain/usecase/GetRemoveWeatherUsecaseTest.kt
+++ domain/src/test/java/com/sangmeebee/weatherlist/domain/usecase/GetRemoveWeatherUsecaseTest.kt
@@ -1,5 +1,7 @@
 package com.sangmeebee.weatherlist.domain.usecase
 
+import com.google.common.truth.Truth.assertThat
+import com.sangmeebee.weatherlist.domain.model.Weather
 import org.junit.Before
 import org.junit.Test
 
@@ -9,20 +11,121 @@ class GetRemoveWeatherUsecaseTest {
 
     @Before
     fun setUp() {
-        getRemoveWeatherUsecase = GetRemoveWeatherUsecase()
+        getRemoveWeatherUsecase = GetRemoveWeatherUsecase(GetCurrentDateUsecase(), ConvertTimestampToDateUsecase())
     }
 
     @Test
     fun `날씨 정보가 오늘 이전의 데이터라면 반환한다`() {
         // given
+        val weathers = listOf(
+            Weather(
+                city = "Seoul",
+                timestamp = 1667617200,
+                tempMin = -2.34,
+                tempMax = 11.63,
+                iconName = "Clear",
+                iconType = "01d"
+            )
+        )
         // when
+        val actual = getRemoveWeatherUsecase(weathers)
         // then
+        assertThat(actual).isEqualTo(weathers)
+    }
+
+    @Test
+    fun `날씨 정보가 오늘 이후의 데이터라면 반환되지 않는다`() {
+        // given
+        val weathers = listOf(
+            Weather(
+                city = "Seoul",
+                timestamp = 4088618200, // 2099-7-25
+                tempMin = -2.34,
+                tempMax = 11.63,
+                iconName = "Clear",
+                iconType = "01d"
+            )
+        )
+        // when
+        val actual = getRemoveWeatherUsecase(weathers)
+        // then
+        assertThat(actual).isEmpty()
     }
 
     @Test
     fun `날씨 정보가 최대 출력 개수를 초과하면 반환한다`() {
         // given
+        val weathers = listOf(
+            Weather(
+                city = "Seoul",
+                timestamp = 4088618200,
+                tempMin = -2.34,
+                tempMax = 11.63,
+                iconName = "Clear",
+                iconType = "01d"
+            ),
+            Weather(
+                city = "Seoul",
+                timestamp = 4088618201,
+                tempMin = -2.34,
+                tempMax = 11.63,
+                iconName = "Clear",
+                iconType = "01d"
+            ),
+            Weather(
+                city = "Seoul",
+                timestamp = 4088618202,
+                tempMin = -2.34,
+                tempMax = 11.63,
+                iconName = "Clear",
+                iconType = "01d"
+            ),
+            Weather(
+                city = "Seoul",
+                timestamp = 4088618203,
+                tempMin = -2.34,
+                tempMax = 11.63,
+                iconName = "Clear",
+                iconType = "01d"
+            ),
+            Weather(
+                city = "Seoul",
+                timestamp = 4088618204,
+                tempMin = -2.34,
+                tempMax = 11.63,
+                iconName = "Clear",
+                iconType = "01d"
+            ),
+            Weather(
+                city = "Seoul",
+                timestamp = 4088618205,
+                tempMin = -2.34,
+                tempMax = 11.63,
+                iconName = "Clear",
+                iconType = "01d"
+            ),
+            Weather(
+                city = "Seoul",
+                timestamp = 4088618206,
+                tempMin = -2.34,
+                tempMax = 11.63,
+                iconName = "Clear",
+                iconType = "01d"
+            )
+        )
         // when
+        val actual = getRemoveWeatherUsecase(weathers)
         // then
+        val expected = listOf(
+            Weather(
+                city = "Seoul",
+                timestamp = 4088618206,
+                tempMin = -2.34,
+                tempMax = 11.63,
+                iconName = "Clear",
+                iconType = "01d"
+            )
+        )
+        assertThat(actual).isEqualTo(expected)
     }
 }
