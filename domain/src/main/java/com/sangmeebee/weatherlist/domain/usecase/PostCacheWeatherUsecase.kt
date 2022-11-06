d3ca95725d01838f1da306a98077e1ed5b430304
Sangmeebee <apfhdznzl@gmail.com>
2022-11-06 21:37:55 +0900

    feat: 날씨 데이터를 캐시로 저장할때 시차에 대한 처리를 한뒤 저장한다

--- domain/src/main/java/com/sangmeebee/weatherlist/domain/usecase/PostCacheWeatherUsecase.kt
+++ domain/src/main/java/com/sangmeebee/weatherlist/domain/usecase/PostCacheWeatherUsecase.kt
@@ -7,6 +7,26 @@ import javax.inject.Inject
 class PostCacheWeatherUsecase @Inject constructor(
     private val weatherRepository: WeatherRepository,
 ) {
-    suspend operator fun invoke(weathers: List<Weather>): Result<Unit> =
-        weatherRepository.insertAllAtCache(weathers)
+    suspend operator fun invoke(weathers: List<Weather>): Result<Unit> {
+        val refinedTimestampWeathers = weathers.map { weather ->
+            weather.copy(
+                timestamp = when (weather.city) {
+                    SEOUL -> weather.timestamp
+                    LONDON -> weather.timestamp + LONDON_TIME_OFFSET - SEOUL_TIME_OFFSET
+                    CHICAGO -> weather.timestamp + CHICAGO_TIME_OFFSET - SEOUL_TIME_OFFSET
+                    else -> weather.timestamp
+                }
+            )
+        }
+        return weatherRepository.insertAllAtCache(refinedTimestampWeathers)
+    }
+
+    companion object {
+        private const val SEOUL = "Seoul"
+        private const val LONDON = "London"
+        private const val CHICAGO = "Chicago"
+        private const val SEOUL_TIME_OFFSET: Long = 32400
+        private const val LONDON_TIME_OFFSET: Long = 0
+        private const val CHICAGO_TIME_OFFSET: Long = -21600
+    }
 }
