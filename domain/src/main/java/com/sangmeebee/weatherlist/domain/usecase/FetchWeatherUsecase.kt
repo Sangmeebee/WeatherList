c7e86448338ba41d911f6a84ccdd1fe4b8ebbfcb
Sangmeebee <apfhdznzl@gmail.com>
2022-11-06 16:28:12 +0900

    feat: 날씨정보 갱신 유스케이스 수정 - 검색하고 싶은 우편번호를 리스트 형태로 수신한다

--- domain/src/main/java/com/sangmeebee/weatherlist/domain/usecase/FetchWeatherUsecase.kt
+++ domain/src/main/java/com/sangmeebee/weatherlist/domain/usecase/FetchWeatherUsecase.kt
@@ -8,16 +8,18 @@ class FetchWeatherUsecase @Inject constructor(
     private val postCacheWeatherUsecase: PostCacheWeatherUsecase,
     private val deleteCacheWeatherUsecase: DeleteCacheWeatherUsecase,
 ) {
-    suspend operator fun invoke(zipCode: String, appId: String): Result<Unit> =
-        getWeatherUsecase(zipCode, appId).fold(
-            onSuccess = { weathers ->
-                postCacheWeatherUsecase(weathers.take(WEATHER_MAX_COUNT)).fold(
-                    onSuccess = { deleteCacheWeatherUsecase() },
-                    onFailure = { Result.failure(PostCacheWeatherException()) }
-                )
-            },
-            onFailure = { Result.failure(it) }
-        )
+    suspend operator fun invoke(zipCodes: List<String>, appId: String): List<Result<Unit>> =
+        zipCodes.map { zipcode ->
+            getWeatherUsecase(zipcode, appId).fold(
+                onSuccess = { weathers ->
+                    postCacheWeatherUsecase(weathers.take(WEATHER_MAX_COUNT)).fold(
+                        onSuccess = { deleteCacheWeatherUsecase() },
+                        onFailure = { Result.failure(PostCacheWeatherException()) }
+                    )
+                },
+                onFailure = { Result.failure(it) }
+            )
+        }
 
     companion object {
         internal const val WEATHER_MAX_COUNT = 6
