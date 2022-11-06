8516fbaec2a733052fa88bf6b2812b1179c23d2c
Sangmeebee <apfhdznzl@gmail.com>
2022-11-06 21:07:51 +0900

    fix: WeatherModel ViewType 추가 & WeatherContent, WeatherTitle로 분리

--- app/src/main/java/com/sangmeebee/weatherlist/model/WeatherModel.kt
+++ app/src/main/java/com/sangmeebee/weatherlist/model/WeatherModel.kt
@@ -1,10 +1,23 @@
 package com.sangmeebee.weatherlist.model
 
-data class WeatherModel(
-    val city: String,
-    val timestamp: Long,
-    val tempMin: Double,
-    val tempMax: Double,
-    val iconName: String,
-    val iconType: String,
-)
+sealed class WeatherModel(open val type: WeatherViewType) {
+
+    data class WeatherTitle(
+        override val type: WeatherViewType,
+        val city: String,
+    ) : WeatherModel(type)
+
+    data class WeatherContent(
+        override val type: WeatherViewType,
+        val city: String,
+        val timestamp: Long,
+        val tempMin: Double,
+        val tempMax: Double,
+        val iconName: String,
+        val iconType: String,
+    ) : WeatherModel(type)
+}
+
+enum class WeatherViewType {
+    TITLE, CONTENT
+}
--- app/src/main/java/com/sangmeebee/weatherlist/model/mapper/DomainToPresentationMapper.kt
+++ app/src/main/java/com/sangmeebee/weatherlist/model/mapper/DomainToPresentationMapper.kt
@@ -2,8 +2,10 @@ package com.sangmeebee.weatherlist.model.mapper
 
 import com.sangmeebee.weatherlist.domain.model.Weather
 import com.sangmeebee.weatherlist.model.WeatherModel
+import com.sangmeebee.weatherlist.model.WeatherViewType
 
-internal fun Weather.toPresentation() = WeatherModel(
+internal fun Weather.toPresentation() = WeatherModel.WeatherContent(
+    type = WeatherViewType.CONTENT,
     city = city,
     timestamp = timestamp,
     tempMin = tempMin,
