9c640d07f810e3ed22a7dd1d209e17ef283bf5e1
Sangmeebee <apfhdznzl@gmail.com>
2022-11-06 17:48:45 +0900

    test: 날씨 로컬 데이터베이스 삭제 유스케이스 테스트 케이스 추가 & 테스트

--- domain/src/test/java/com/sangmeebee/weatherlist/domain/usecase/DeleteCacheWeatherUsecaseTest.kt
+++ domain/src/test/java/com/sangmeebee/weatherlist/domain/usecase/DeleteCacheWeatherUsecaseTest.kt
@@ -1,19 +1,89 @@
 package com.sangmeebee.weatherlist.domain.usecase
 
 import com.google.common.truth.Truth.assertThat
-import com.sangmeebee.weatherlist.domain.fake.FakeWeatherRepository
+import com.sangmeebee.weatherlist.domain.model.Weather
+import com.sangmeebee.weatherlist.domain.repository.WeatherRepository
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import io.mockk.spyk
 import kotlinx.coroutines.test.runTest
+import org.junit.Before
 import org.junit.Test
 
 class DeleteCacheWeatherUsecaseTest {
+
+    private lateinit var deleteCacheWeatherUsecase: DeleteCacheWeatherUsecase
+    private lateinit var weatherRepository: WeatherRepository
+    private lateinit var getRemoveWeatherUsecase: GetRemoveWeatherUsecase
+
+    @Before
+    fun setUp() {
+        weatherRepository = mockk(relaxed = true)
+        getRemoveWeatherUsecase = mockk()
+        deleteCacheWeatherUsecase = spyk(DeleteCacheWeatherUsecase(weatherRepository, getRemoveWeatherUsecase))
+    }
+
     @Test
-    fun `날씨 정보가 오늘 이전의 데이터거나 최대 출력개수를 초과하면 삭제한다`() = runTest {
+    fun `로컬 데이터베이스에 저장된 날씨 정보를 불러온다`() = runTest {
         // given
-        val deleteCacheWeatherUsecase =
-            DeleteCacheWeatherUsecase(FakeWeatherRepository(), GetRemoveWeatherUsecase(GetCurrentDateUsecase(), ConvertTimestampToDateUsecase()))
+        coEvery { deleteCacheWeatherUsecase() } returns Result.success(Unit)
+        // when
+        deleteCacheWeatherUsecase()
+        // then
+        coVerify { weatherRepository.getAllWeathersAtCache() }
+    }
+
+    @Test
+    fun `로컬 데이터베이스에 저장된 날씨 정보를 불러오는 것을 실패하면 예외를 반환한다`() = runTest {
+        // given
+        coEvery { weatherRepository.getAllWeathersAtCache() } returns Result.failure(Throwable())
         // when
         val actual = deleteCacheWeatherUsecase()
         // then
-        assertThat(actual.isSuccess).isTrue()
+        assertThat(actual.isFailure).isTrue()
+    }
+
+    @Test
+    fun `받아온 날씨 정보 중 오늘 이전의 데이터거나 최대 출력개수를 초과한 데이터가 있다면 그 정보들을 삭제한다`() = runTest {
+        // given
+        val removedWeathers = listOf(
+            Weather(
+                city = "Seoul",
+                timestamp = 1667617200,
+                tempMin = -2.34,
+                tempMax = 11.63,
+                iconName = "Clear",
+                iconType = "01d"
+            )
+        )
+        coEvery { weatherRepository.getAllWeathersAtCache() } returns Result.success(mapOf("Seoul" to removedWeathers))
+        coEvery { getRemoveWeatherUsecase(removedWeathers) } returns removedWeathers
+        // when
+        deleteCacheWeatherUsecase()
+        // then
+        coVerify { weatherRepository.deleteWeathersAtCache(removedWeathers) }
+    }
+
+    @Test
+    fun `정보를 삭제하는데 실패하면 예외를 반환한다`() = runTest {
+        // given
+        val removedWeathers = listOf(
+            Weather(
+                city = "Seoul",
+                timestamp = 1667617200,
+                tempMin = -2.34,
+                tempMax = 11.63,
+                iconName = "Clear",
+                iconType = "01d"
+            )
+        )
+        coEvery { weatherRepository.getAllWeathersAtCache() } returns Result.success(mapOf("Seoul" to removedWeathers))
+        coEvery { getRemoveWeatherUsecase(removedWeathers) } returns removedWeathers
+        coEvery { weatherRepository.deleteWeathersAtCache(removedWeathers) } returns Result.failure(Throwable())
+        // when
+        val actual = deleteCacheWeatherUsecase()
+        // then
+        assertThat(actual.isFailure).isTrue()
     }
 }
