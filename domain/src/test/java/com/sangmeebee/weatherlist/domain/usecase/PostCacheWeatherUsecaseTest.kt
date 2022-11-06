8dc88050e8e5e8cfaca2733e169c21f3b67a365d
Sangmeebee <apfhdznzl@gmail.com>
2022-11-07 02:01:55 +0900

    test: 날씨를 로컬 데이터베이스에 저장하는 유스케이스 테스트 케이스 추가

--- domain/src/test/java/com/sangmeebee/weatherlist/domain/usecase/PostCacheWeatherUsecaseTest.kt
+++ domain/src/test/java/com/sangmeebee/weatherlist/domain/usecase/PostCacheWeatherUsecaseTest.kt
@@ -3,15 +3,34 @@ package com.sangmeebee.weatherlist.domain.usecase
 import com.google.common.truth.Truth.assertThat
 import com.sangmeebee.weatherlist.domain.fake.FakeWeatherRepository
 import com.sangmeebee.weatherlist.domain.model.Weather
+import io.mockk.spyk
+import io.mockk.verify
 import kotlinx.coroutines.test.runTest
+import org.junit.Before
 import org.junit.Test
 
 class PostCacheWeatherUsecaseTest {
 
+    private lateinit var postCacheWeatherUsecase: PostCacheWeatherUsecase
+    private lateinit var convertTimestampInSeoul: ConvertTimestampInSeoul
+
+    @Before
+    fun setUp() {
+        convertTimestampInSeoul = spyk(ConvertTimestampInSeoul())
+        postCacheWeatherUsecase = PostCacheWeatherUsecase(FakeWeatherRepository(), convertTimestampInSeoul)
+    }
+
+    @Test
+    fun `날씨 정보의 unix timpstamp를 서울기준으로 변경한다`() = runTest {
+        // when
+        postCacheWeatherUsecase(emptyList())
+        // then
+        verify { convertTimestampInSeoul(emptyList()) }
+    }
+
     @Test
     fun `날씨 리스트를 저장한다`() = runTest {
         // given
-        val postCacheWeatherUsecase = PostCacheWeatherUsecase(FakeWeatherRepository())
         val weathers = listOf(
             Weather(
                 city = "Seoul",
