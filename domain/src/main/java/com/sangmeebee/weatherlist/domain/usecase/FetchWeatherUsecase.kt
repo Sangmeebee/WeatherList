cda72bf9b42b7b9934d18288e12342d9f8403961
Sangmeebee <apfhdznzl@gmail.com>
2022-11-07 15:39:36 +0900

    fix: FetchWeatherUsecase 불러온 날씨목록을 도시별로 따로 캐시에 저장 -> 한번에 캐시에 저장으로 수정

--- domain/src/main/java/com/sangmeebee/weatherlist/domain/usecase/FetchWeatherUsecase.kt
+++ domain/src/main/java/com/sangmeebee/weatherlist/domain/usecase/FetchWeatherUsecase.kt
@@ -1,5 +1,6 @@
 package com.sangmeebee.weatherlist.domain.usecase
 
+import com.sangmeebee.weatherlist.domain.model.Weather
 import javax.inject.Inject
 
 class FetchWeatherUsecase @Inject constructor(
@@ -7,18 +8,27 @@ class FetchWeatherUsecase @Inject constructor(
     private val postCacheWeatherUsecase: PostCacheWeatherUsecase,
     private val deleteCacheWeatherUsecase: DeleteCacheWeatherUsecase,
 ) {
-    suspend operator fun invoke(zipCodes: List<String>, appId: String): List<Result<Unit>> =
+    suspend operator fun invoke(zipCodes: List<String>, appId: String): List<Result<Unit>> {
+        val postWeathers = mutableListOf<Weather>()
+        val results = mutableListOf<Result<Unit>>()
         zipCodes.map { zipcode ->
-            getWeatherUsecase(zipcode, appId).fold(
-                onSuccess = { weathers ->
-                    postCacheWeatherUsecase(weathers.take(WEATHER_MAX_COUNT)).fold(
-                        onSuccess = { deleteCacheWeatherUsecase() },
-                        onFailure = { throwable -> Result.failure(throwable) }
-                    )
-                },
-                onFailure = { Result.failure(it) }
-            )
+            getWeatherUsecase(zipcode, appId)
+                .onSuccess { weathers ->
+                    postWeathers.addAll(weathers.take(WEATHER_MAX_COUNT))
+                }
+                .onFailure { throwable ->
+                    results.add(Result.failure(throwable))
+                }
         }
+
+        results.add(
+            postCacheWeatherUsecase(postWeathers).fold(
+                onSuccess = { deleteCacheWeatherUsecase() },
+                onFailure = { throwable -> Result.failure(throwable) }
+            )
+        )
+        return results
+    }
 
     companion object {
         internal const val WEATHER_MAX_COUNT = 6
